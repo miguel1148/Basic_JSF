4d6a0350024ee13854144d5daad044e40d221a7f
Miguel <miguelramalho51@gmail.com>
2019-03-02 13:00:47 -0300

    primeiro commit

---
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -0,0 +1,25 @@
+package br.com.cursojsf;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "pessoaBean")
+public class PessoaBean {
+	
+	private String nome;
+	private String sobreNome;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+
+}
