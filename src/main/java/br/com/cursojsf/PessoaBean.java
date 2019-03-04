b5d40a0372d46196392b1ceb02ab8de233ea52db
Miguel <miguelramalho51@gmail.com>
2019-03-04 09:44:15 -0300

    Navegação Dinâmica

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,35 +1,66 @@
 package br.com.cursojsf;
 
-import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+
+//@RequestScoped
+//@SessionScoped
+//@ApplicationScoped
+
+@ViewScoped
 @ManagedBean(name = "pessoaBean")//
 public class PessoaBean {
 	
 	private String nome;
-	private String sobreNome;
-	
-	private String nomeCompleto;
 	
 	
+	private HtmlCommandButton commandButton;
+	private List<String> nomes = new ArrayList<String>();
 	
-	public String getNomeCompleto() {
-		this.nomeCompleto = nome + " " + sobreNome;
-		return nomeCompleto;
+	
+	
+	public String addNome() {
+		nomes.add(nome);
+		if(nomes.size() > 3) {
+			return "paginaNavegada";//navegação dinâmica
+		}
+		
+		return "";
 	}
 	
-	
-	
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+
+	public HtmlCommandButton getCommandButton() {
+		return commandButton;
+	}
+
+	public void setCommandButton(HtmlCommandButton commandButton) {
+		this.commandButton = commandButton;
+	}
+
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getSobreNome() {
-		return sobreNome;
-	}
-	public void setSobreNome(String sobreNome) {
-		this.sobreNome = sobreNome;
-	}
+	
+	
+	
+	
 
 }
